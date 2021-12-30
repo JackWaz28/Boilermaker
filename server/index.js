247752cea109d155d96c0ee1a05c79c9c0e91f1b
speedturtle28 <jackw4@vt.edu>
2021-12-30 17:21:37 +0000

    Deleted main folder and changed apiRoutes to api

--- server/index.js
+++ server/index.js
@@ -1,23 +1,52 @@
-const path = require('path')
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
+const { db } = require('./db')
+const PORT = process.env.PORT || 8080
+const app = require('./app')
+const seed = require('../script/seed');
+
+const init = async () => {
+  try {
+    if(process.env.SEED === 'true'){
+      await seed();
+    }
+    else {
+      await db.sync()
+    }
+    // start listening (and create a 'server' object representing our server)
+    app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
+  } catch (ex) {
+    console.log(ex)
+  }
+}
+
+init()
 
 
-app.use(morgan('dev'));
+// const path = require('path')
+// const express = require('express');
+// const app = express();
+// const morgan = require('morgan');
 
-app.use(express.json())
 
-app.use(express.static(path.join(__dirname, '../public')));
+// app.use(morgan('dev'));
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.json())
 
-app.use('/api', require('./apiRoutes')); // matches all requests to /api
+// app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, './path/to/index.html'));
-})
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-module.exports = app
+// app.use('/api', require('./apiRoutes')); // matches all requests to /api
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../index.html'));
+// })
+
+// app.use((err, req, res, next) => {
+//     console.error(err);
+//     console.error(err.stack);
+//     res.status(err.status || 500).send(err.message || 'Internal server error.');
+// });
+
+// module.exports = app
